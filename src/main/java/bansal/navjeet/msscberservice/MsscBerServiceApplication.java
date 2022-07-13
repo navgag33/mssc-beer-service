5aedd1d9b2c2fa6d346feadd5ca4f2d33377eb27
Navjeet Bansal <navgag33@gmail.com>
2022-07-13 12:50:56 +0530

    Initial Commit LC Coding with SFG

---
+++ src/main/java/bansal/navjeet/msscberservice/MsscBerServiceApplication.java
@@ -0,0 +1,13 @@
+package bansal.navjeet.msscberservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bansal/navjeet/msscberservice/MsscBerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package bansal.navjeet.msscberservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
