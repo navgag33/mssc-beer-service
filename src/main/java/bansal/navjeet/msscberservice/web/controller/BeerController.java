8d21a8fd68b0474d0f08e039098b009661e203ac
Navjeet Bansal <navgag33@gmail.com>
2022-07-13 18:43:43 +0530

    Tests Added for the project!

--- src/main/java/bansal/navjeet/msscberservice/web/controller/BeerController.java
+++ src/main/java/bansal/navjeet/msscberservice/web/controller/BeerController.java
@@ -13,7 +13,7 @@ public class BeerController {
 
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId ){
-        return new ResponseEntity<>( HttpStatus.CREATED);
+        return new ResponseEntity<>( BeerDto.builder().build(), HttpStatus.OK);
     }
     @PostMapping
     public ResponseEntity<BeerDto> saveNewBeer(@RequestBody BeerDto beerDto){
@@ -23,6 +23,6 @@ public class BeerController {
 
     @PutMapping("/{beerId}")
     public ResponseEntity<BeerDto> updateBeer(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
-        return new ResponseEntity<>(beerDto, HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(beerDto, HttpStatus.NO_CONTENT);
     }
 }
